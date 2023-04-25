10c10fa65debb51614dc9fe6ac45e0d5eaf37454
ayosefiguera <ayo.afa@gmail.com>
2023-04-25 18:59:39 +0100

    fix nex appointment widget from home

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,8 +1,5 @@
-import 'package:eqlibrum/facade/impl/default_appointment_facade.dart';
-import 'package:eqlibrum/models/appointment.dart';
-import 'package:eqlibrum/utils/utils.dart';
+import 'package:eqlibrum/views/widgets/next_appointment_widget.dart';
 import 'package:eqlibrum/views/widgets/scaffold_app.dart';
-import 'package:eqlibrum/views/themes/themes.dart';
 import 'package:eqlibrum/views/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,9 +7,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final  DefaultAppointmentFacade appointmentFacade = DefaultAppointmentFacade();
     return ScaffoldApp(
-        index:1,
+        index: 1,
         child: SizedBox(
           width: double.infinity,
           height: double.infinity,
@@ -35,7 +31,8 @@ class HomeScreen extends StatelessWidget {
                     ),
                     ElevatedButton(
                       style: ButtonStyle(
-                        padding: MaterialStateProperty.all(const EdgeInsets.all(8)),
+                        padding:
+                            MaterialStateProperty.all(const EdgeInsets.all(8)),
                       ),
                       onPressed: () => Navigator.pushNamed(context, 'schelude'),
                       child: const Text(
@@ -48,17 +45,7 @@ class HomeScreen extends StatelessWidget {
                 const SizedBox(
                   height: 12,
                 ),
-                appointmentFacade.isLoading
-                    ? const Center(
-                        child: SizedBox(
-                            width: 100,
-                            height: 100,
-                            child: CircularProgressIndicator(
-                              color: AppTheme.primary,
-                            )),
-                      )
-                    : _NextAppointment(
-                        appointment: appointmentFacade.getEventsForDay(kToday)),
+                NextAppointment(),
                 const SizedBox(
                   height: 18,
                 ),
@@ -80,79 +67,5 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
         ));
-  }
-}
-
-class _NextAppointment extends StatelessWidget {
-  const _NextAppointment({required this.appointment});
-
-  final List<Appointment> appointment;
-
-  @override
-  Widget build(BuildContext context) {
-    Color statusColor = AppTheme.primary;
-
-    return appointment.isEmpty
-        ? const Text("No appointment today")
-        : Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: AppTheme.primary,
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-            child: Column(
-              children: [
-                SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                    "Next Appointment. Day: ${appointment[0].date.day} of ${appointment[0].date.month}",
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                        fontSize: 14,
-                        color: Colors.white70,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                    padding: const EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                        color: Colors.white70,
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border.all(
-                            color: statusColor,
-                            style: BorderStyle.solid,
-                            width: 2)),
-                    child: Row(
-                      children: [
-                        const Icon(
-                          Icons.access_time_outlined,
-                          color: Colors.black54,
-                        ),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        Text(
-                          "${appointment[0].date.day}:${appointment[0].date.minute}",
-                          style: const TextStyle(color: Colors.black54),
-                        ),
-                        const SizedBox(
-                          width: 18,
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              appointment[0].psychologistID,
-                              style: const TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.w600),
-                            ),
-                          ],
-                        ),
-                      ],
-                    )),
-              ],
-            ),
-          );
   }
 }
---
+++ lib/views/widgets/next_appointment_widget.dart
@@ -0,0 +1,112 @@
+import 'dart:collection';
+import 'package:eqlibrum/utils/utils.dart';
+import 'package:eqlibrum/facade/appointment_facade.dart';
+import 'package:eqlibrum/facade/impl/default_appointment_facade.dart';
+import 'package:eqlibrum/models/appointment.dart';
+import 'package:eqlibrum/utils/utils.dart';
+import 'package:eqlibrum/views/themes/themes.dart';
+import 'package:eqlibrum/views/widgets/scaffold_app.dart';
+import 'package:flutter/material.dart';
+
+class NextAppointment extends StatelessWidget {
+  NextAppointment({super.key});
+  final AppointmentFacade appointmentFacade = DefaultAppointmentFacade();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+        future: appointmentFacade.loadAppointment(''),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: CircularProgressIndicator(
+                    color: AppTheme.primary,
+                  )),
+            );
+          } else {
+            if (snapshot.data == true) {
+              return _NextAppointment(
+                  appointment: appointmentFacade.getEventsForDay(kToday));
+            } else {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
+          }
+        });
+  }
+}
+
+class _NextAppointment extends StatelessWidget {
+  _NextAppointment({required this.appointment});
+
+  final List<Appointment> appointment;
+
+  @override
+  Widget build(BuildContext context) {
+    Color statusColor = AppTheme.primary;
+
+    return appointment.isEmpty
+        ? const Text("No appointment today")
+        : Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: AppTheme.primary,
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    "Next Appointment. Day: ${appointment[0].date.day} of ${appointment[0].date.month}",
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.white70,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                        color: Colors.white70,
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                            color: statusColor,
+                            style: BorderStyle.solid,
+                            width: 2)),
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.access_time_outlined,
+                          color: Colors.black54,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "${appointment[0].date.day}:${appointment[0].date.minute}",
+                          style: const TextStyle(color: Colors.black54),
+                        ),
+                        const SizedBox(
+                          width: 18,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              appointment[0].psychologistID,
+                              style: const TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w600),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+          );
+  }
+}
